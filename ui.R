5e7f9bf4d2ae8b5f43ab268cf76fb556fc3ff3b4
melcam0 <camillomelzi@gmail.com>
2025-11-10 19:24:56 +0100

    aggiornamento esempi

--- ui.R
+++ ui.R
@@ -68,7 +68,7 @@ sidebar<- dashboardSidebar(
                          br(),br(),
                          actionButton("quit", "Quit",onclick = "setTimeout(function(){window.close();},200);",
                                       style='padding:4px; font-size:80%'),
-                         HTML('<p><center><font color="cyan"><br> Version 8.5 </font></center>')
+                         HTML('<p><center><font color="cyan"><br> Version 8.6 </font></center>')
                         )))
 
 
